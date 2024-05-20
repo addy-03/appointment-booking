be686c2415a380451257bfa5a8c9b75c1f0bc334
Aditya <adityawagh169@gmail.com>
2024-05-20 22:53:59 +0530

    Added Calender Component and axios call to API

--- src/components/AppointmentScheduler/index.jsx
+++ src/components/AppointmentScheduler/index.jsx
@@ -1,6 +1,44 @@
+import { useEffect, useState } from "react";
 import "./styles.css";
+import CustomCalender from "../Calender";
+import axios from "axios";
 
 const AppointmentScheduler = () => {
+  const [value, onChange] = useState(new Date());
+  const [availableSlots, setAvailableSlots] = useState(null);
+
+  const getAvailableSlots = async (date) => {
+    axios
+      .get(
+        `https://app.appointo.me/scripttag/mock_timeslots?start_date=${date?.startDate}&end_date=${date?.endDate}`
+      )
+      .then((res) => setAvailableSlots(res.data))
+      .catch((e) => console.log({ e }));
+  };
+
+  useEffect(() => {
+    const startDate = new Date(value);
+    let endDate = new Date(value);
+    endDate.setDate(endDate.getDate() + 1);
+
+    console.log({
+      value,
+    });
+
+    getAvailableSlots({
+      startDate: `${startDate.getFullYear()}-${
+        startDate.getMonth() + 1
+      }-${startDate.getDate()}`,
+      endDate: `${endDate.getFullYear()}-${
+        endDate.getMonth() + 1
+      }-${endDate.getDate()}`,
+    });
+  }, [value]);
+
+  useEffect(() => {
+    console.log({ availableSlots });
+  }, [availableSlots]);
+
   return (
     <div className="flex-col appointment-container">
       <div className="flex-row body">
@@ -12,9 +50,13 @@ const AppointmentScheduler = () => {
               <span className="timezone-value">Asia/Calcutta</span>
             </div>
           </div>
-          <div className="calender-wrapper">Calender Grid</div>
+          <div className="calender-wrapper">
+            <CustomCalender value={value} onChange={onChange} />
+          </div>
         </div>
-        <div className="slots-wrapper"></div>
+        <div className="slots-wrapper">{`${availableSlots
+          ?.at(0)
+          ?.slots?.reduce((a, b) => (a += "\n" + b.start_time))}`}</div>
       </div>
       <div className="footer">
         <span className="text">
---
+++ src/components/Calender/index.jsx
@@ -0,0 +1,8 @@
+import Calender from "react-calendar";
+import "./styles.css";
+
+const CustomCalender = (props) => {
+  return <Calender {...props} />;
+};
+
+export default CustomCalender;
